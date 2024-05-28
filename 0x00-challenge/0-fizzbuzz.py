c8acd7c40065df660869dd2e22f3701506095ce0
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-05-28 15:46:04 +0100

    Added files for Task 3

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -12,20 +12,21 @@ def fizzbuzz(n):
 	 multiples of five print "Buzz".
 	- For numbers which are multiples of both three and five print "FizzBuzz".
 	"""
-	if n < 1:
-		return
 
-	tmp_result = []
-	for i in range(1, n + 1):
-		if (i % 3) == 0 and (i % 5) == 0:
-			tmp_result.append("FizzBuzz")
-		elif (i % 3) == 0:
-			tmp_result.append("Fizz")
-		elif (i % 5) == 0:
-			tmp_result.append("Buzz")
-		else:
-			tmp_result.append(str(i))
-	print(" ".join(tmp_result))
+    if n < 1:
+        return
+
+    tmp_result = []
+    for i in range(1, n + 1):
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
 
 
 if __name__ == '__main__':
